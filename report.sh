8233846de23cfe4bf0b1dda491dffb50f03809b9
Motko <77466238+Motko222@users.noreply.github.com>
2025-09-25 08:40:25 +0200

    Update report.sh

--- report.sh
+++ report.sh
@@ -10,6 +10,7 @@ version=$()
 service=$(sudo systemctl status $folder --no-pager | grep "active (running)" | wc -l)
 errors=$(journalctl -u $folder.service --since "1 hour ago" --no-hostname -o cat | grep -c -E "rror|ERR")
 last=$(journalctl -u $folder.service --no-hostname -o cat | grep -E "Received CheckNodeOperation request" | tail -1 | cut -d "\"" -f 2)
+address=$(journalctl -u $folder.service --no-hostname -o cat | grep -E "Verified identity result" | tail -1 | awk -F "address=" '{print $NF}')
 
 status="ok" && message=""
 [ $errors -gt 500 ] && status="warning" && message="errors=$errors";
