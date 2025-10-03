28cd77cab2d9aced5ff5e83f0a3c04200f11a861
Motko <77466238+Motko222@users.noreply.github.com>
2025-10-03 21:49:47 +0200

    Update report.sh

--- report.sh
+++ report.sh
@@ -12,6 +12,21 @@ errors=$(journalctl -u $folder.service --since "1 hour ago" --no-hostname -o cat
 last=$(journalctl -u $folder.service --no-hostname -o cat | grep -E "Received CheckNodeOperation request" | tail -1 | cut -d "\"" -f 2)
 wallet=$(journalctl -u $folder.service --no-hostname -o cat | grep -E "Verified identity result" | tail -1 | awk -F "address=" '{print $NF}')
 
+timestamp=$(date -d "$last" +%s)
+now=$(date +%s)
+
+# Calculate difference in seconds
+diff=$(( $(date +%s) - $(date -d "$last" +%s) ))
+
+if [ $diff -lt 3600 ]; then
+  last_ago="$(( diff / 60 )) minutes ago"
+elif [ $diff -lt 86400 ]; then
+  last_ago="$(( diff / 3600 )) hours ago"
+else
+  last_ago="$(( diff / 86400 )) days ago"
+fi
+
+
 status="ok" && message=""
 [ $errors -gt 500 ] && status="warning" && message="errors=$errors";
 [ $service -ne 1 ] && status="error" && message="service not running";
@@ -35,7 +50,7 @@ cat >$json << EOF
         "service":"$service",
         "errors":"$errors",
         "height":"",
-        "m1":"last=$last",
+        "m1":"last=$last_ago",
         "m2":"",
         "m3":"",
         "url":"$ZG_ALIGNMENT_NODE_SERVICE_IP",
