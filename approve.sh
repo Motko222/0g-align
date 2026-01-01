8ff39135ef4deb546485bf7f86ac6d44635be8ef
Motko <77466238+Motko222@users.noreply.github.com>
2026-01-01 21:00:32 +0100

    Update approve.sh

--- approve.sh
+++ approve.sh
@@ -6,5 +6,5 @@ read -p "Address? " address
 read -p "Token ids? " ids
 
 cd /root/$folder
-./0g-alignment-node approve --key $ZG_ALIGNMENT_NODE_SERVICE_PRIVATEKEY --tokenIds $ids --chain-id 42161 \
- --rpc https://arb1.arbitrum.io/rpc --contract 0xdD158B8A76566bC0c342893568e8fd3F08A9dAac  --destNode $address
+./0g-alignment-node approve --key $ZG_ALIGNMENT_NODE_SERVICE_PRIVATEKEY --tokenIds $ids \
+--chain-id 16661 --contract 0x7BDc2aECC3CDaF0ce5a975adeA1C8d84Fd9Be3D9 --rpc "https://evmrpc.0g.ai" --destNode $address
